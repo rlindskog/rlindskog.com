5d5151d8d1bcdaa31b012a7bc5c389aed31a6703
Ryan Lindskog <r.lindskog17@gmail.com>
2019-10-09 11:30:15 -0700

    added sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon-32x32.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response !== undefined || !(event.request.url.indexOf('http') === 0)) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return response
+      });
+    }
+  }));
+});
